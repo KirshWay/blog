a77b0c388bc3d73b0eb2edc4716c250f75d37193
Victor <nemecvictor.kirsh@gmail.com>
2021-12-08 12:32:03 +0300

    test solution

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,12 +1,36 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 import { Container, Heading } from '@chakra-ui/react';
 import AnimationLayout from '../layouts/AnimationLayout';
 
-const Posts: NextPage = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+// @ts-ignore
+const Posts: NextPage = ({ allPostsData }) => {
   return (
     <AnimationLayout>
       <Container>
         <Heading as="h3">Posts</Heading>
+        <ul>
+          {/*@ts-ignore*/}
+          {allPostsData.map(({ id, title, date }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <p>{date}</p>
+            </li>
+          ))}
+        </ul>
       </Container>
     </AnimationLayout>
   );
